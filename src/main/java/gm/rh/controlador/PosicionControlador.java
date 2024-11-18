e16596cfbbef54f9e94691733d8e6bcf84afd4fe
Laptop <memorosas.00@gmail.com>
2024-11-17 19:46:08 -0600

    y funciona la insercion de nuevas funciones.

--- src/main/java/gm/rh/controlador/PosicionControlador.java
+++ src/main/java/gm/rh/controlador/PosicionControlador.java
@@ -12,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +52,15 @@ public class PosicionControlador {
 
         return ResponseEntity.ok(posiciones);
     }
+
+    @PostMapping("/posiciones")
+    public Posicion agregarActualizarPosicion(@RequestBody Posicion posicionToSave){
+            
+        logger.info("La posicion a agregar/actulizar es: " + posicionToSave);
+            
+        return posicionServicio.guardarPosicion(posicionToSave);
+    }
+
 
     // @GetMapping("/largoplazoo")
     // public <List<Posicion>> listarFinDePosicionesLargoPlazo22(@PathVariable("fechaInicial") LocalDate fechaInicial, @PathVariable ("fechaFinal") LocalDate fechaFinal){
