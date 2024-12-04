9ffd138f5b7418c68bfb9e6ca96420400bf928a0
Laptop <memorosas.00@gmail.com>
2024-12-03 22:25:16 -0600

    test, se agrega fecha server

--- src/main/java/gm/rh/modelo/Posicion.java
+++ src/main/java/gm/rh/modelo/Posicion.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;      // con ello se crea la tabla bd en autma
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
 import lombok.AllArgsConstructor;
 import lombok.Data;                     // para crear los get y setters automarticos.
 import lombok.NoArgsConstructor;        // consturctor sin argumentos
@@ -37,6 +38,14 @@ public class Posicion {
     // Afuerza hay que usar este tipo de nombre sin "_" para que no falle la JPA Convention
     Boolean largoPlazo;
     LocalDateTime fechaCapturaFront;
+
+
+    // LocalDateTime fechaServer;
+    // @PrePersist
+    // public void prePersist() {
+    //     this.fechaServer = LocalDateTime.now(); // Asignar la fecha y hora actual
+    // }
+
      
     // Instrumento 	Plazo 	Registro 	Dia de Subasta 	Fecha Final 	Reinversion 	Monto 	ISR Ret 	Rendimiento %
 }
